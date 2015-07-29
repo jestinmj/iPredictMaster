e0f8a33caeebc03aa24cfdd3ebc1aa506538672f
DanHenton <Dan.henton@live.com>
2015-07-29 15:00:05 +1200

    more functionality added to portfolio as well as coloring elements

--- www/js/portfolioCtrl.js
+++ www/js/portfolioCtrl.js
@@ -1,6 +1,13 @@
 angular.module('app.controllers.portfolio', ["chart.js"])
 
     .controller('PortfolioCtrl', function($scope) {
+
+        $scope.block={
+            ownStock:true,
+            shortStock: true
+        };
+
+
         $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
         $scope.data = [
             [65, 59, 80, 81, 56, 55, 40],
@@ -9,4 +16,13 @@ angular.module('app.controllers.portfolio', ["chart.js"])
         $scope.onClick = function (points, evt) {
             console.log(points, evt);
         };
+
+        $scope.toggleOwnStock = function() {
+            $scope.block.ownStock = !$scope.block.ownStock;
+        };
+
+        $scope.toggleShortStock = function(){
+            $scope.block.shortStock = !$scope.block.shortStock;
+        };
+
     });
