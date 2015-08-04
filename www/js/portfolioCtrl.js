c56b9bc71eeef7abb45467b16328ed64ea526722
DanHenton <Dan.henton@live.com>
2015-08-05 10:05:01 +1200

    connected the font end of the portfolio page to the controller changed the styling for the stocks and shorts

--- www/js/portfolioCtrl.js
+++ www/js/portfolioCtrl.js
@@ -7,6 +7,20 @@ angular.module('app.controllers.portfolio', ["chart.js"])
             shortStock: true
         };
 
+        $scope.portfolio={
+            myRank:1,
+            changeInRank:2,
+            myWorth:20.05,
+            changeInWorth:0.07,
+            myWallet:12.74,
+            changeInWallet:"NC",
+            myPortfolio:7.39,
+            changeInPortfolio:0.07
+        };
+
+        $scope.myStocks = ["Stock One", "Stock Two", "Stock Three"];
+
+        $scope.myShorts = ["Short One","Short Two","Short Three"];
 
         $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
         $scope.data = [
@@ -24,5 +38,6 @@ angular.module('app.controllers.portfolio', ["chart.js"])
         $scope.toggleShortStock = function(){
             $scope.block.shortStock = !$scope.block.shortStock;
         };
+
 
     });
