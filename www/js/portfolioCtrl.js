0af419dd7ad029370e102b7c1658b3c146db083e
DanHenton <Dan.henton@live.com>
2015-08-12 16:26:38 +1200

    added graph and data

--- www/js/portfolioCtrl.js
+++ www/js/portfolioCtrl.js
@@ -28,6 +28,7 @@ angular.module('app.controllers.portfolio', ["chart.js"])
             [65, 59, 80, 81, 56, 55, 40],
             [28, 48, 40, 19, 86, 27, 90]
         ];
+
         $scope.onClick = function (points, evt) {
             console.log(points, evt);
         };
