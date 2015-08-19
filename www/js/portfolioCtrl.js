3ca866d0992c25a12cd2cdf3295c9ca31abb356a
DanHenton <Dan.henton@live.com>
2015-08-19 16:44:12 +1200

    adding michaels code -getting the contracts and the browsing linked as contracts now take in the data

--- www/js/portfolioCtrl.js
+++ www/js/portfolioCtrl.js
@@ -19,24 +19,14 @@ angular.module('app.controllers.portfolio', ["chart.js"])
             changeInPortfolio:0.07
         };
 
-        $scope.myStocks = [
-            {title:"GOLD.15APR15.HI",amount:2, value:"$"+0.09, profit:"+$"+0.051, averageCost:"+$"+0.00023, lastPrice:"$"+0.07500, difference:"+$"+0.01409},
-            {title:"GOLD.15APR15.LOW",amount:1, value:"$"+0.23, profit:"+$"+0.032, averageCost:"+$"+0.00023, lastPrice:"$"+0.07500, difference:"+$"+0.01409},
-            {title:"GOLD.15APR15.MID",amount:3, value:"$"+0.65, profit:"+$"+0.454, averageCost:"+$"+0.00023, lastPrice:"$"+0.07500, difference:"+$"+0.01409},
-            {title:"GOLD.15APR15.VHI",amount:5, value:"$"+0.86, profit:"+$"+0.934, averageCost:"+$"+0.00023, lastPrice:"$"+0.07500, difference:"+$"+0.01409}
-        ];
+        $scope.myStocks = ["Stock One", "Stock Two", "Stock Three"];
 
-        $scope.myShorts = [
-            {title:"GOLD.15APR15.HI",amount:1, value:"$"+0.10, profit:"+$"+0.433, averageCost:"+$"+0.00023, lastPrice:"$"+0.07500, difference:"+$"+0.01409},
-            {title:"GOLD.15APR15.LOW",amount:3, value:"$"+0.02, profit:"+$"+0.653, averageCost:"+$"+0.00023, lastPrice:"$"+0.07500, difference:"+$"+0.01409},
-            {title:"GOLD.15APR15.MID",amount:4, value:"$"+0.34, profit:"+$"+0.064, averageCost:"+$"+0.00023, lastPrice:"$"+0.07500, difference:"+$"+0.01409},
-            {title:"GOLD.15APR15.VHI",amount:6, value:"$"+0.32, profit:"+$"+0.567, averageCost:"+$"+0.00023, lastPrice:"$"+0.07500, difference:"+$"+0.01409}
-
-        ];
+        $scope.myShorts = ["Short One","Short Two","Short Three"];
 
         $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
         $scope.data = [
-            [23, 12, 6, 7, 4, 3, 1],
+            [65, 59, 80, 81, 56, 55, 40],
+            [28, 48, 40, 19, 86, 27, 90]
         ];
 
         $scope.onClick = function (points, evt) {
