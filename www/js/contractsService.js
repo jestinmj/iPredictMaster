4028b4b5ff35f85c227de673104f3a50575cbe20
DanHenton <Dan.henton@live.com>
2015-09-23 16:19:59 +1200

    edit contracts titles and get all contracts function now returns a list

--- www/js/contractsService.js
+++ www/js/contractsService.js
@@ -22,51 +22,51 @@ angular.module('app.services.contract', [])
 
         // contractID : contract obj
 
-            "1" : {"buy": 1.20, "catId": 0, "dateModified": "00/00/00", "featuredText": "string", "id": "0001", "imageName": "string", "judgeDate": "00/00/00",
+            "1" : {"buy": 1.20, "catId": 0, "dateModified": "00/00/00", "featuredText": "string", "id": "0001", "imageName": "gold", "judgeDate": "00/00/00",
             "judgeStatement": "string", "last": 1.09, "longDesc": "string", "marketId": 0, "name": "GOLD.15APR15.HI","sell": 1.15, "shortDesc": "Will Gold prices rise", "startDate": "00/00/00",
             "tagIds": {
                     "tagId": [0]}, "type": 0 },
-            "2" : {"buy": 0.89, "catId": 0, "dateModified": "00/00/00", "featuredText": "string", "id": "0002", "imageName": "string", "judgeDate": "00/00/00",
+            "2" : {"buy": 0.89, "catId": 0, "dateModified": "00/00/00", "featuredText": "string", "id": "0002", "imageName": "gold", "judgeDate": "00/00/00",
                 "judgeStatement": "string", "last": 0.88, "longDesc": "string", "marketId": 0, "name": "GOLD.15APR15.LOW","sell":0.92, "shortDesc": "Will Gold prices Fall", "startDate": "00/00/00",
                 "tagIds": {
                     "tagId": [0]}, "type": 0 },
-            "3" : {"buy": 0.89, "catId": 0, "dateModified": "00/00/00", "featuredText": "string", "id": "0003", "imageName": "string", "judgeDate": "00/00/00",
+            "3" : {"buy": 0.89, "catId": 0, "dateModified": "00/00/00", "featuredText": "string", "id": "0003", "imageName": "gold", "judgeDate": "00/00/00",
                 "judgeStatement": "string", "last": 0.83, "longDesc": "string", "marketId": 0, "name": "GOLD.15APR15.MID","sell":0.75, "shortDesc": "Will Gold prices stay the same", "startDate": "00/00/00",
                 "tagIds": {
                     "tagId": [0]}, "type": 0 },
-            "4" : {"buy": 1.23, "catId": 0, "dateModified": "00/00/00", "featuredText": "string", "id": "0004", "imageName": "string", "judgeDate": "00/00/00",
+            "4" : {"buy": 1.23, "catId": 0, "dateModified": "00/00/00", "featuredText": "string", "id": "0004", "imageName": "johnKey", "judgeDate": "00/00/00",
                 "judgeStatement": "string", "last": 1.20, "longDesc": "string", "marketId": 0, "name": "JOHN.KEY.REMAINS.2015","sell":1.24, "shortDesc": "Will John Key Remain New Zealands prime minister in 2016", "startDate": "00/00/00",
                 "tagIds": {
                     "tagId": [0]}, "type": 0 },
-            "5" : {"buy": 0.49, "catId": 0, "dateModified": "00/00/00", "featuredText": "string", "id": "0005", "imageName": "string", "judgeDate": "00/00/00",
+            "5" : {"buy": 0.49, "catId": 0, "dateModified": "00/00/00", "featuredText": "string", "id": "0005", "imageName": "money", "judgeDate": "00/00/00",
                 "judgeStatement": "string", "last": 0.48, "longDesc": "string", "marketId": 0, "name": "NZD.FALL.BELLOW.0.70","sell":0.42, "shortDesc": "Will NZD continue to fall bellow $0.70", "startDate": "00/00/00",
                 "tagIds": {
                     "tagId": [0]}, "type": 0 },
-            "6" : {"buy": 0.20, "catId": 0, "dateModified": "00/00/00", "featuredText": "string", "id": "0006", "imageName": "string", "judgeDate": "00/00/00",
+            "6" : {"buy": 0.20, "catId": 0, "dateModified": "00/00/00", "featuredText": "string", "id": "0006", "imageName": "money", "judgeDate": "00/00/00",
                 "judgeStatement": "string", "last": 0.18, "longDesc": "string", "marketId": 0, "name": "NZD.RAISE.ABOVE.0.70","sell":0.18, "shortDesc": "Will NZD raise back up to above $0.70", "startDate": "00/00/00",
                 "tagIds": {
                     "tagId": [0]}, "type": 0 },
-            "7" : {"buy":0.35, "catId": 0, "dateModified": "00/00/00", "featuredText": "string", "id": "0007", "imageName": "string", "judgeDate": "00/00/00",
+            "7" : {"buy":0.35, "catId": 0, "dateModified": "00/00/00", "featuredText": "string", "id": "0007", "imageName": "leakyHome", "judgeDate": "00/00/00",
                 "judgeStatement": "string", "last": 0.35, "longDesc": "string", "marketId": 0, "name": "LEAKY.HOME.COST.2M","sell":0.32, "shortDesc": "Will the Leaking home cost the government 200 million in repair costs in 2015", "startDate": "00/00/00",
                 "tagIds": {
                     "tagId": [0]}, "type": 0 },
-            "8" : {"buy":0.55, "catId": 0, "dateModified": "00/00/00", "featuredText": "string", "id": "0008", "imageName": "string", "judgeDate": "00/00/00",
+            "8" : {"buy":0.55, "catId": 0, "dateModified": "00/00/00", "featuredText": "string", "id": "0008", "imageName": "leakyHome", "judgeDate": "00/00/00",
                 "judgeStatement": "string", "last": 0.54, "longDesc": "string", "marketId": 0, "name": "LEAKY.HOME.COST.5M","sell":0.52, "shortDesc": "Will the Leaking home cost the government 500 million in repair costs in 2015", "startDate": "00/00/00",
                 "tagIds": {
                     "tagId": [0]}, "type": 0 },
-            "9" : {"buy":0.35, "catId": 0, "dateModified": "00/00/00", "featuredText": "string", "id": "0009", "imageName": "string", "judgeDate": "00/00/00",
+            "9" : {"buy":0.35, "catId": 0, "dateModified": "00/00/00", "featuredText": "string", "id": "0009", "imageName": "earthquake", "judgeDate": "00/00/00",
                 "judgeStatement": "string", "last": 0.35, "longDesc": "string", "marketId": 0, "name": "WELL.EARTHQUAKE.LOW","sell":0.32, "shortDesc": "Will Wellington area receive a 4.0 to 4.8 Earthquake in before the end of 2015", "startDate": "00/00/00",
                 "tagIds": {
                     "tagId": [0]}, "type": 0 },
-            "10" : {"buy":0.35, "catId": 0, "dateModified": "00/00/00", "featuredText": "string", "id": "0010", "imageName": "string", "judgeDate": "00/00/00",
+            "10" : {"buy":0.35, "catId": 0, "dateModified": "00/00/00", "featuredText": "string", "id": "0010", "imageName": "earthquake", "judgeDate": "00/00/00",
                 "judgeStatement": "string", "last": 0.35, "longDesc": "string", "marketId": 0, "name": "WELL.EARTHQUAKE.MID","sell":0.32, "shortDesc": "Wellington to receive a 4.9 to 5.4 Earthquake in before the end of 2015", "startDate": "00/00/00",
                 "tagIds": {
                     "tagId": [0]}, "type": 0 },
-            "11" : {"buy":0.35, "catId": 0, "dateModified": "00/00/00", "featuredText": "string", "id": "0011", "imageName": "string", "judgeDate": "00/00/00",
+            "11" : {"buy":0.35, "catId": 0, "dateModified": "00/00/00", "featuredText": "string", "id": "0011", "imageName": "earthquake", "judgeDate": "00/00/00",
                 "judgeStatement": "string", "last": 0.35, "longDesc": "string", "marketId": 0, "name": "WELL.EARTHQUAKE.HI","sell":0.32, "shortDesc": "Wellington to receive a 5.5 to 5.8 Earthquake in before the end of 2015", "startDate": "00/00/00",
                 "tagIds": {
                     "tagId": [0]}, "type": 0 },
-            "12" : {"buy":0.35, "catId": 0, "dateModified": "00/00/00", "featuredText": "string", "id": "0012", "imageName": "string", "judgeDate": "00/00/00",
+            "12" : {"buy":0.35, "catId": 0, "dateModified": "00/00/00", "featuredText": "string", "id": "0012", "imageName": "earthquake", "judgeDate": "00/00/00",
                 "judgeStatement": "string", "last": 0.35, "longDesc": "string", "marketId": 0, "name": "WELL.EARTHQUAKE.VHI","sell":0.32, "shortDesc": "Wellington to receive an Earthquake above 5.8 in before the end of 2015", "startDate": "00/00/00",
                 "tagIds": {
                     "tagId": [0]}, "type": 0 }
